a997229ced20d1773cc85bf6f68d22cad80a0c1a
Ahmed Wageh <ahmed.wageh@cajs-awageh-x13.local>
2019-05-30 21:36:04 -0400

    complete

---
+++ Assets/Spin2Win.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin2Win : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(speed, 0f, 0f));
+    }
+}
