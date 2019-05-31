cba0883361d8da87161ae68a18d3dc2c4e3373a4
Ahmed Wageh <ahmedsamywageh@gmail.com>
2019-05-30 22:53:17 -0400

    better vid & other stuff

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Drive : MonoBehaviour
 {
@@ -15,6 +16,9 @@ public class Drive : MonoBehaviour
     void Update()
     {
         transform.position += transform.forward * (speed / Time.deltaTime);
-        // transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, startPosition.y + Random.value * 100f, transform.localPosition.z), 0.1f);
+
+        if(Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene("demoscene");
+        }
     }
 }
