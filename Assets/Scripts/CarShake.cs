1d41aab08ec2edd28d273096f97d6602cd88e2c9
Ahmed Wageh <ahmedsamywageh@gmail.com>
2019-05-30 22:18:40 -0400

    final & media

---
+++ Assets/Scripts/CarShake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarShake : MonoBehaviour
+{
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, initialPosition.y + Random.value * 2f, transform.localPosition.z), .5f * Time.deltaTime);
+        transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, new Vector3(Random.value * 10f, transform.localEulerAngles.y, transform.localEulerAngles.z), .2f * Time.deltaTime);
+    }
+}
